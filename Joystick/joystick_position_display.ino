94fb9aed69024430cfb5cd56a3d9fd597af90520
Siddharth Sivakumar <dharthsid.sivakumar@gmail.com>
2026-07-23 22:14:31 -0400

    Update joystick_position_display.ino documentation 1

--- Joystick/joystick_position_display.ino
+++ Joystick/joystick_position_display.ino
@@ -1,3 +1,11 @@
+// Siddharth Sivakumar
+// July 22, 2026
+// Arduino Mega
+/* The purpose of this program is to use a joystick 
+to control the current position within a 1023x1023 
+plane and output to an LCD display (LCD1602 Module). 
+The joystick can be pressed to release a warning sound. */
+
 #include <LiquidCrystal.h>
 int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -5,7 +13,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int switchPin = 7;
 int xPosPin = A8;
 int yPosPin = A9;
-int buzzPin = 6;
+int buzzerPin = 6;
 
 String outputString;
 int xPos;
@@ -17,7 +25,7 @@ void setup() {
   pinMode(switchPin, INPUT);
   pinMode(xPosPin, INPUT);
   pinMode(yPosPin, INPUT);
-  pinMode(buzzPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
   Serial.begin(9600);
   digitalWrite(switchPin, HIGH);
 
@@ -36,10 +44,10 @@ void loop() {
   lcd.setCursor(0, 1);
   if (switchButton == 0) {
     lcd.print("ON");
-    digitalWrite(buzzPin, HIGH);
+    digitalWrite(buzzerPin, HIGH);
   } else {
     lcd.print("OFF");
-    digitalWrite(buzzPin, LOW);
+    digitalWrite(buzzerPin, LOW);
   }
   delay(delayTime);
   lcd.clear();
