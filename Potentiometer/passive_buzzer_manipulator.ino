be5c5d0e9e5fedd8f16110bf983ddb5a25fb2a23
Siddharth Sivakumar <dharthsid.sivakumar@gmail.com>
2026-07-24 22:25:20 -0400

    Update passive_buzzer_manipulator.ino doc1

--- Potentiometer/passive_buzzer_manipulator.ino
+++ Potentiometer/passive_buzzer_manipulator.ino
@@ -1,4 +1,9 @@
-int buzzPin = 9; // Can be modified or changed to match current pin of Arduino
+// Siddharth Sivakumar
+// July 1, 2026
+// Arduino MEGA
+// The purpose of this program
+
+int buzzerPin = 9; // Can be modified or changed to match current pin of Arduino
 int analogPin = A9; // Can be modified or changed to match current pin of Arduino
 
 float finalDelay = 0;
@@ -8,21 +13,18 @@ int delayTimeHigh = 1000;
 int delayTimeLow = 60;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(buzzPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
   pinMode(analogPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   tenBitNum = analogRead(analogPin);
-
   finalDelay = ((delayTimeHigh-delayTimeLow)/(1023.0)) * tenBitNum + delayTimeLow;
 
-  digitalWrite(buzzPin, HIGH);
+  digitalWrite(buzzerPin, HIGH);
   delayMicroseconds(finalDelay);
-  digitalWrite(buzzPin, LOW);
+  digitalWrite(buzzerPin, LOW);
   delayMicroseconds(finalDelay);
   Serial.print("The delay is ");
   Serial.print(finalDelay);
